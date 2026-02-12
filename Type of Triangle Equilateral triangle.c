726023cf243476407c47790faaaa102db351a279
dhanushnandhakumar023-dev <dhanushnandhakumar023@gmail.com>
2026-02-12 11:37:17 +0530

    Create Type of Triangle Equilateral triangle.c

---
+++ Type of Triangle Equilateral triangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if  (a+b+c==15)
+    {
+        printf("Equilateral triangle");
+    }
+    else
+    {
+    printf("Not Equilateral triangle");
+    }
+    return 0;
+}
