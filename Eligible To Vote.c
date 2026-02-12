c88855285105e0f0d788b6f3f09b9df7d19486ce
dhanushnandhakumar023-dev <dhanushnandhakumar023@gmail.com>
2026-02-12 09:41:12 +0530

    Create Eligible To Vote.c

---
+++ Eligible To Vote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int Age;
+    scanf("%d",&Age);
+    if  (Age >= 18)
+    {
+        printf("Eligible To Vote");
+    }
+    else
+    {
+        printf("Not  Eligible To Vote");
+    }
+    return 0;
+}
